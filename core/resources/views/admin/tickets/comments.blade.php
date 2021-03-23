4a70dce56d140486b408d58f1f7ff47239f0cfa8
Zakarialabib <58374576+Zakarialabib@users.noreply.github.com>
2021-03-23 16:11:24 +0100

    tickets changes

--- core/resources/views/admin/tickets/comments.blade.php
+++ core/resources/views/admin/tickets/comments.blade.php
@@ -4,7 +4,7 @@
     <div class="flex relative mt-6" style=" margin-left:10px;">
         <i class="fas fa-user text-grey-dark"></i> 
         <div class=" ml-2 "> 
-          <p class="font-medium text-sm text-grey-darkest">{{ $comment->created_at->format('Y-m-d H:m') }}</p>
+          <p class="font-medium text-sm text-grey-darkest">{{ $ticket->user->name}} {{ $comment->created_at->format('Y-m-d H:m') }}</p>
           <small class="bg-green-300 float-right w-3/4 mx-4 my-2 p-2 rounded-lg clearfix">{{ $comment->comment }}</small>
         </div>
         <hr>
